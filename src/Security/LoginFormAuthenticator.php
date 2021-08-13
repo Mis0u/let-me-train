110ef1efa3ad3df401d4e193e0d5c9da32447ad3
Misou <micka75bup@gmail.com>
2021-08-13 22:17:08 +0200

    Création du LoginForm

---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use App\Helper\Service\RedirectUserConnectedService;
+use App\Repository\UserRepository;
+use Cocur\Slugify\Slugify;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
+{
+    use TargetPathTrait;
+
+    public const LOGIN_ROUTE = 'app_login';
+
+    private UrlGeneratorInterface $urlGenerator;
+    private UserRepository $userRepository;
+    private RedirectUserConnectedService $redirectUserConnectedService;
+
+    public function __construct(
+        UrlGeneratorInterface $urlGenerator,
+        UserRepository $userRepository,
+        RedirectUserConnectedService $redirectUserConnectedService
+    ) {
+        $this->urlGenerator                 = $urlGenerator;
+        $this->userRepository               = $userRepository;
+        $this->redirectUserConnectedService = $redirectUserConnectedService;
+    }
+
+    public function authenticate(Request $request): PassportInterface
+    {
+        $email = $request->request->get('email', '');
+
+        $request->getSession()->set(Security::LAST_USERNAME, $email);
+
+        return new Passport(
+            new UserBadge((string) $email),
+            new PasswordCredentials((string) $request->request->get('password', '')),
+            [
+                new CsrfTokenBadge('authenticate', $request->get('_csrf_token')),
+            ]
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+            return new RedirectResponse($targetPath);
+        }
+
+        if ($token->getUser() instanceof User) {
+            return $this->redirectUserConnectedService->redirectToProfile($this->userRepository, $token->getUser());
+        }
+
+        return new RedirectResponse($this->urlGenerator->generate(self::LOGIN_ROUTE));
+    }
+
+    protected function getLoginUrl(Request $request): string
+    {
+        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+}
